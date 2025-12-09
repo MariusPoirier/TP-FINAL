37356658e734506f8622f91ab663b3e0c9601cc5
MariusPoirier <164481347+MariusPoirier@users.noreply.github.com>
2025-12-09 13:26:21 +0100

    Add files via upload

--- application/src/game/core/charactere/cube.c
+++ application/src/game/core/charactere/cube.c
@@ -1,19 +1,95 @@
 #include "cube.h"
 
-Cube *Cube_create(char face)
+Face* Face_create(int code)
 {
-    assert(face != NULL);
+    Face* p = (Face*)calloc(1, sizeof(Face));
+    p->code = code;
+    return p;
+    
+}
+Cube *Cube_create()
+{
 
     Cube *cube = (Cube *)calloc(1, sizeof(Cube));
     AssertNew(cube);
 
-    cube->actual_face = face;
+    
+    cube->under = Face_create(1);
+    cube->right = Face_create(2);
+    cube->left = Face_create(3);
+    cube->behind = Face_create(4);
+    cube->front = Face_create(5);
+    cube->over = Face_create(6);
+
+
     cube->has_hache=false;
     cube->has_key=false;
 
     return cube;
 }
 
+void Cube_print(Cube* cube)
+{
+    printf("under\t: %d\nover\t: %d\nleft\t: %d\nbehind\t: %d\nright\t: %d\nfront\t: %d\n\n"
+        , cube->under->code, cube->over->code, cube->left->code, cube->behind->code, cube->right->code, cube->front->code);
+}
+
+void Cube_rota_right(Cube* cube)
+{
+    Face* temp = cube->left;
+    cube->left = cube->behind;
+    cube->behind = cube->right;
+    cube->right = cube->front;
+    cube->front = temp;
+
+}
+
+void Cube_rota_left(Cube* cube)
+{
+    Face* temp = cube->left;
+    cube->left = cube->front;
+    cube->front = cube->right;
+    cube->right = cube->behind;
+    cube->behind = temp;
+
+}
+
+void Cube_left(Cube* cube)
+{
+    Face* temp = cube->under;
+    cube->under = cube->left;
+    cube->left = cube->over;
+    cube->over = cube->right;
+    cube->right = temp;
+}
+
+void Cube_behind(Cube* cube)
+{
+    Face* temp = cube->under;
+    cube->under = cube->behind;
+    cube->behind = cube->over;
+    cube->over = cube->front;
+    cube->front = temp;
+
+}
+void Cube_right(Cube* cube)
+{
+    Face* temp = cube->under;
+    cube->under = cube->right;
+    cube->right = cube->over;
+    cube->over = cube->left;
+    cube->left = temp;
+
+}
+void Cube_front(Cube* cube)
+{
+    Face* temp = cube->under;
+    cube->under = cube->front;
+    cube->front = cube->over;
+    cube->over = cube->behind;
+    cube->behind = temp;
+
+}
 bool Has_Hache(Cube *self){
     return (self->has_hache);
 }
--- application/src/game/core/charactere/cube.h
+++ application/src/game/core/charactere/cube.h
@@ -1,20 +1,57 @@
 #pragma once
 
 #include "settings.h"
+typedef struct Face
+{
+    // 1) trou clé
+    // 2) hache
+    // 3) bouclier
+    // 4) backpack
+    // 5) face
+    // 6) head
+    int code;
+
+} Face;
 
 typedef struct Cube
 {
-    char actual_face;
+   
+    Face* under;
+    Face* right;
+    Face* left;
+    Face* behind;
+    Face* front;
+    Face* over;
+
     bool has_hache;
     bool has_key;
 
 } Cube;
 
+
+/// @brief Crée une face.
+/// 
+/// @param face la face sur lequel le dé doit être.
+/// @return Le cube créée.
+Face * Face_create(int code);
 /// @brief Crée un cube.
 /// 
 /// @param face la face sur lequel le dé doit être.
 /// @return Le cube créée.
-Cube *Cube_create(char face);
+Cube *Cube_create();
+
+void Cube_print(Cube *cube);
+
+void Cube_rota_right(Cube* cube);
+void Cube_rota_left(Cube* cube);
+
+void Cube_left(Cube *cube);
+void Cube_behind(Cube* cube);
+void Cube_right(Cube* cube);
+void Cube_front(Cube* cube);
+
+
+
 
 /// @brief Dis si le cube à la hache.
 /// 
